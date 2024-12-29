8fe832a355dfa75c431e627b7352fae361f371f3
Aubakirov Eraly <eraly02112006@gmail.com>
2024-12-29 16:41:53 +0500

    Telegramm Bot first version

--- Bot.py
+++ Bot.py
@@ -1,1 +1,17 @@
-print('Привет')
+import telebot
+import webbrowser
+
+bot = telebot.TeleBot('7857313253:AAGAIT7rTbs5ey66lwt8ASxHOL76zEufaZs')
+
+@bot.message_handler(commands=['start']) 
+def main(message):
+    bot.send_message(message.chat.id , f'Привет, {message.from_user.first_name}')
+    
+@bot.message_handler() 
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+
+
+
+bot.polling(non_stop=True)
